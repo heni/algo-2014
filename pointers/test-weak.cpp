77262f3e06b2def08db2e4ad4828800daf847197
Eugene Krokhalev <eugene.krokhalev@gmail.com>
2014-09-19 12:48:49 +0400

    more correct code

--- pointers/test-weak.cpp
+++ pointers/test-weak.cpp
@@ -4,11 +4,17 @@
 struct TA;
 
 class TB {
-    std::shared_ptr<TA> Parent;
+    std::weak_ptr<TA> Parent;
 public:
     TB(const std::shared_ptr<TA>& parent) 
         : Parent(parent)
     {}
+    void PrintPointers() {
+        std::shared_ptr<TA> parent = Parent.lock();
+        std::cout << "created objects (from child):" << std::endl
+            << "\tparent: " << parent.get() << std::endl
+            << "\tchild: " << this << std::endl;
+    }
     ~TB() {
         std::cout << "~TB()" << std::endl;
     }
@@ -26,8 +32,9 @@ int main() {
     std::shared_ptr<TA> ptr2parent = std::make_shared<TA>();
     std::shared_ptr<TB> ptr2child = std::make_shared<TB>(ptr2parent);
     ptr2parent->Child = ptr2child;
-    std::cout << "created objects:" << std::endl
+    std::cout << "created objects (from main):" << std::endl
         << "\tparent: " << ptr2parent.get() << std::endl
         << "\tchild: " << ptr2child.get() << std::endl;
+    ptr2child->PrintPointers();
     return 0;
 }
